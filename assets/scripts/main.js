b96535c6a31aa3f6de5a7e8149fe59f257f868c7
Paul Goldman <paulgoldmanmusic@gmail.com>
2017-08-30 23:12:08 -0500

    Added jWeather to portfolio page

---
+++ assets/scripts/main.js
@@ -0,0 +1,13 @@
+/*global alert, document, $ */
+
+(function () {
+    "use strict";
+    $(document).ready(function () {
+
+        $(".nav a").on("click", function () {
+            $(".nav").find(".active").removeClass("active");
+            $(this).parent().addClass("active");
+        });
+
+    });
+}());
